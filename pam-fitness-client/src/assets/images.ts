543114c260e7e3dc67980ad2c174ee5e9294ae85
BrianXYZ <brianchima22@gmail.com>
2025-05-20 10:35:48 +0100

    compressed image file to improve page load time

--- pam-fitness-client/src/assets/images.ts
+++ pam-fitness-client/src/assets/images.ts
@@ -1,7 +1,7 @@
 export { default as heroImg } from "./images/hero-page.jpg";
-export {default as servicesImg1} from "./images/qym-cable-pull-workout.jpg"; 
+export {default as servicesImg1} from "./images/gym-cable-pull-workout.jpg"; 
 export {default as servicesImg2} from "./images/gym-treadmill-workout.jpg"; 
-export {default as servicesImg3} from "./images/squat-workout.jpg"; 
+export {default as joinUsImg} from "./images/joinUsImg.jpg"; 
 // export {default as servicesImg1} from ""; 
 export {default as servicesImg5} from "./images/biceps-workout.jpg"; 
 export {default as communityImg} from "./images/gym-community.jpg"; 
